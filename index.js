f3b314a5d99727601e4e29eeb3da927930860aa1
Jake <zamorejake@gmail.com>
2023-05-26 14:23:23 -0500

    Got shapes generating, need to adjust spacing and add inquirer logic

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
 const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
-
-const svg = `<svg version="1.1"
-width="300" height="200"
-xmlns="http://www.w3.org/2000/svg">
-
-<rect width="100%" height="100%" fill="blue" />
-
-<circle cx="150" cy="100" r="80" fill="green" />
-
-<text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
-
-</svg>`
-
+const { Triangle, Square, Circle } = require('./lib/shapes.js');
+const shape = new Circle();
+shape.background = "blue";
+shape.shapeColor = "red";
+shape.textColor = "purple";
+shape.text = "SVG";
+const shape2 = shape.renderCircle();
 const examples = './examples'; // Specify the path to the desired folder
 const outputFilePath = path.join(examples, 'output.svg');
 fs.mkdirSync(examples, { recursive: true });
-fs.writeFile(outputFilePath, svg, (err) => {
+fs.writeFile(outputFilePath, shape2, (err) => {
     if (err) {
       console.error(err);
       return;
