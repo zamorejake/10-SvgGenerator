ddfabe2e171d4746a65c1d35b5bae95de5a093ec
Jake <zamorejake@gmail.com>
2023-05-26 19:12:45 -0500

    Fixed spacing on svgs

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const inquirer = require("inquirer");
 const { Triangle, Square, Circle } = require('./lib/shapes.js');
 const shape = new Circle();
 shape.background = "blue";
-shape.shapeColor = "red";
+shape.shapeColor = "teal";
 shape.textColor = "purple";
 shape.text = "SVG";
-const shape2 = shape.renderCircle();
-const examples = './examples'; // Specify the path to the desired folder
+const shape2 = shape.renderShape();
+const examples = './examples';
 const outputFilePath = path.join(examples, 'output.svg');
 fs.mkdirSync(examples, { recursive: true });
 fs.writeFile(outputFilePath, shape2, (err) => {
